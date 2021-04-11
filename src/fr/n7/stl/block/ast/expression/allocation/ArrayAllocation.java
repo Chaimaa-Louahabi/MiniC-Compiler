94d17a15f3703e16fade71a5864301f5d977664d
Louahabi Chaimaa <clouahab@dazzler.enseeiht.fr>
2021-04-12 00:22:40 +0200

    ArrayAllocation done

--- src/fr/n7/stl/block/ast/expression/allocation/ArrayAllocation.java
+++ src/fr/n7/stl/block/ast/expression/allocation/ArrayAllocation.java
@@ -4,12 +4,14 @@
 package fr.n7.stl.block.ast.expression.allocation;
 
 import fr.n7.stl.block.ast.SemanticsUndefinedException;
+import fr.n7.stl.block.ast.expression.BinaryOperator;
 import fr.n7.stl.block.ast.expression.Expression;
 import fr.n7.stl.block.ast.scope.Declaration;
 import fr.n7.stl.block.ast.scope.HierarchicalScope;
 import fr.n7.stl.block.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
 import fr.n7.stl.tam.ast.TAMFactory;
+import fr.n7.stl.tam.ast.Library;
 import fr.n7.stl.block.ast.type.ArrayType;
 
 /**
@@ -63,7 +65,13 @@ public class ArrayAllocation implements Expression {
 	 */
 	@Override
 	public Fragment getCode(TAMFactory _factory) {
-		throw new SemanticsUndefinedException( "Semantics getCode is undefined in ArrayAllocation.");
+		Fragment frag = _factory.createFragment();
+		frag.append(this.size.getCode(_factory));// LOADL x
+		frag.add(_factory.createLoadL(this.getType().length()));
+		frag.add(TAMFactory.createBinaryOperator(BinaryOperator.Multiply));
+		frag.add(Library.MAlloc);
+		frag.addComment("ArrayAllocation");
+		return frag;
 	}
 
 }
